e10c358988131311c971a6d8102efd02eed78f9e
jweir136 <jweir136@yahoo.com>
2022-01-26 20:48:30 -0500

    add main notifications

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-let { app, BrowserWindow } = require("electron");
+let { app, BrowserWindow, Notification } = require("electron");
 
 function createWindow () {
     const win = new BrowserWindow({
       width: 800,
       height: 600
     });
+
+    new Notification({"title":"TITLE", "body":"BODY"});
   
     win.loadFile('index.html');
   }
