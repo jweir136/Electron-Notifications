8bb9968be41737d5dc529dda26527e2540165e95
jweir136 <jweir136@yahoo.com>
2022-01-28 09:14:12 -0500

    add drawImg function

--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
+let img = new Image();
 
 function setCanvasDimensions() {
     canvas.width = 300;
@@ -12,5 +13,13 @@ function drawRectangle(height, width) {
     context.stroke();
 }
 
+function drawImg() {
+        img.onload = function(e) {
+            context.drawImage(img, 10, 10);
+        }
+        img.src = "pic.jpg"    
+}
+
 setCanvasDimensions();
-drawRectangle(150, 150);
+//drawRectangle(150, 150);
+drawImg();
