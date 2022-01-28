6f2882aa8677f1635f50750961319eae7dabb5d3
jweir136 <jweir136@yahoo.com>
2022-01-27 21:28:20 -0500

    add getWebcamAccess()

--- renderer.js
+++ renderer.js
@@ -6,3 +6,17 @@ function hasGetUserMedia()
 {
     return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
+
+function getWebcamAccess()
+{
+    const constraints = {
+        video: true,
+    };
+      
+    const video = document.querySelector("video");
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        video.srcObject = stream;
+    });
+}
+
+getWebcamAccess();
