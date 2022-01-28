ba8bea52fa5303e453331c142ef9ea24fde046ea
jweir136 <jweir136@yahoo.com>
2022-01-28 15:20:54 -0500

    init

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let { app, BrowserWindow } = require("electron");
+const bodyPix = require('@tensorflow-models/body-pix');
 
 function createWindow () {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+      width: 1000,
+      height: 1000
     });
   
     win.loadFile('index.html');
--- renderer.js
+++ renderer.js
@@ -1,27 +1,51 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
+const image = document.querySelector("img");
+let img = new Image();
 
-const video = document.querySelector("video");
-const constraints = {
-    video: true,
-};
 
-const FPS = 10; // change this to set the video frame refresh rate.
-
-setInterval(draw, FPS);
-
-function draw()
+function drawImageOnCanvas()
 {
-    context.drawImage(video, 0, 0);
-    //context.clearRect(0, 0, canvas.width, canvas.height);
-
+    canvas.width = 5000;
+    canvas.height = 1000;
+    img.onload = function(e) {
+        context.drawImage(img, 0, 0);
+    }
+    img.src = "person.png";
 }
 
-function getWebcamAccess()
+function drawBodyPart(x, y)
 {
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-        video.srcObject = stream;
-    });
+    context.fillStyle = "red";
+
+    context.beginPath();
+    context.rect(x, y, 5, 5);
+    context.fill();
 }
 
+
+async function getWebcamAccess()
+{
+    // load the BodyPix model from a checkpoint
+    const net = await bodyPix.load();
+    const segmentation = await net.segmentPerson(canvas);
+
+    for (let i = 0; i < segmentation.allPoses.length; i+=1)
+    {
+        for (let j = 0; j < segmentation.allPoses[i].keypoints.length; j+=1)
+        {
+            let part = segmentation.allPoses[i].keypoints[j].part;
+            let x = segmentation.allPoses[i].keypoints[j].position.x;
+            let y = segmentation.allPoses[i].keypoints[j].position.y;
+            let score = segmentation.allPoses[i].keypoints[j].score;
+            
+            if (score >= 0.75)
+            {
+                drawBodyPart(x, y);
+            }
+        }
+    }
+}
+
+drawImageOnCanvas();
 getWebcamAccess();
